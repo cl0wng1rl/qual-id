bbec8897efce08300703d46a3eda1087af4adba0
Gabriel Barker <gabrielbarker@gmail.com>
2020-09-23 10:20:45 +0100

    REFACTOR Pattern class

--- src/Pattern.py
+++ src/Pattern.py
@@ -1,5 +1,9 @@
+from random import choice
+
 class Pattern:
-  category_map = {
+  __random_key = "random"
+
+  __category_map = {
     "animal": "data/animal.txt",
     "food": "data/food.txt",
   }
@@ -8,13 +12,32 @@ class Pattern:
     self.categories = pattern_string.split('-')
 
   def is_valid(self):
-    if not (0 < len(self.categories) < 6):
+    if not self.__categories_length_is_acceptable():
       return False
+    return self.__categories_all_exist()
+  
+  def get_paths(self):
+    return [Pattern.__category_map[category] for category in self.categories]
+
+  def __categories_length_is_acceptable(self):
+    return (0 < len(self.categories) < 6)
+  
+  def __get_path_from_category(self, category):
+    if category == Pattern.__random_key:
+      category = Pattern.__random_category()
+    return Pattern.__category_map[category]
+  
+  def __categories_all_exist(self):
     for category in self.categories:
-      path = Pattern.category_map[category]
+      path = self.__get_path_from_category(category)
       if not path:
         return False
     return True
+
+  @staticmethod
+  def get_categories(self):
+    return Pattern.__category_map.keys()
   
-  def get_paths(self):
-    return [Pattern.category_map[category] for category in self.categories]
+  @staticmethod
+  def __random_category(self):
+    return choice(Pattern.get_categories())
