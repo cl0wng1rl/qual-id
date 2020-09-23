48e97735b19812cb43e74981d7f6da977863e44c
Gabriel Barker <gabrielbarker@gmail.com>
2020-09-23 21:54:20 +0100

    ALLOW number param in url

--- app.py
+++ app.py
@@ -7,14 +7,19 @@ app = Flask(__name__)
 
 @app.route('/get/', methods=['GET'])
 def respond():
-    pattern = Pattern(request.args.get("pattern", ""))  
+    pattern = Pattern(request.args.get("pattern", ""))
+    number = int(request.args.get("number", 1))
+
     response = {}
 
     if not pattern.is_valid():
         response["error"] = "pattern is invalid"
     else:
-        response["data"] = get_qual_id(pattern)
+        response["data"] = get_qual_ids(pattern, number)
     return jsonify(response)
+
+def get_qual_ids(pattern, number):
+  return [get_qual_id(pattern) for _ in range(number)]
 
 def get_qual_id(pattern):
   return '-'.join([path.get_random_value() for path in pattern.get_categories()])
