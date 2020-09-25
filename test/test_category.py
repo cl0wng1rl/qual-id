26005fb5eaee944efcf276c60515984683727036
Gabriel Barker <gabrielbarker@gmail.com>
2020-09-25 01:30:33 +0100

    REFACTOR category test

--- test/test_category.py
+++ test/test_category.py
@@ -1,18 +1,9 @@
 import unittest
 from qual_id.category import Category
-import unittest.mock as mock
 import random
 
 
-def fixed_choice(lst):
-  print('in')
-  print(lst)
-  return TestCategory.__random_choice
-
-
 class TestCategory(unittest.TestCase):
-  __random_choice = 'random_choice'
-
   def setUp(self):
     self.category = Category()
 
@@ -21,7 +12,11 @@ class TestCategory(unittest.TestCase):
 
   def test__get_random_choice__mock_random_choice__returns_empty_string(self):
     random.seed(0)
-    self.assertEqual(self.category.get_random_value(), '')
+    fixed_choice = self.__get_random_choice_from_values()
+    self.assertEqual(self.category.get_random_value(), fixed_choice)
+
+  def __get_random_choice_from_values(self):
+    return random.choice(self.category.get_values())
 
 
 if __name__ == '__main__':
