17046994e82bfaa4b6285da51ba7056afa211917
aldinaufal21 <ry.prg75@gmail.com>
2020-09-29 20:04:40 +0700

    added a new category drink

---
+++ qual_id/categories/drink.py
@@ -0,0 +1,14 @@
+from ..category import Category
+
+class Drink(Category):
+    def get_values:
+        return[
+            'juice',
+            'milk',
+            'beer',
+            'water',
+            'coffee',
+            'tea',
+            'wine',
+            'chocolate'
+        ]
