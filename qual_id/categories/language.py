adc102f187ee8bcf5dcffb3cd447e6f02c089a96
jaqsparow <jaqsparow@yahoo.com>
2020-10-01 22:23:38 +0530

    Add new category language

---
+++ qual_id/categories/language.py
@@ -0,0 +1,25 @@
+from ..category import Category
+
+
+class Language(Category):
+    def get_values(self):
+        return[
+            'English',
+            'Hindi',
+            'Spanish',
+            'French',
+            'Bengali',
+            'Russian',
+            'German',
+            'Japanese',
+            'Urdu',
+            'Tamil',
+            'Turkish',
+            'Korean',
+            'Italian',
+            'Thai',
+            'Persian',
+            'Filipino',
+            'Indonesian'
+
+        ]
