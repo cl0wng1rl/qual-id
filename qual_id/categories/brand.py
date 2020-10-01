85254d8bd908698394d7dc535badef33d8d017c7
jaqsparow <jaqsparow@yahoo.com>
2020-10-01 22:58:53 +0530

    Add new category brand

---
+++ qual_id/categories/brand.py
@@ -0,0 +1,27 @@
+from ..category import Category
+
+
+class Brand(Category):
+    def get_values(self):
+        return[
+            'Apple',
+            'Samsung',
+            'Nokia',
+            'Tesla',
+            'Infiniti',
+            'Hisense',
+            'Adidas'
+            'Puma',
+            'Nike',
+            'Flora',
+            'Reliance',
+            'Airtel',
+            'Oppo',
+            'Dyson',
+            'Bush',
+            'Amazon',
+            'Intel',
+            'Sandisk',
+            'Nvidia',
+            'Reebok'
+        ]
