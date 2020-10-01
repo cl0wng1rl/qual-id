0b2e3d61baffe3ae78f7a78e149f0e5580faf423
N Balgopal Patro <46865367+BalgopalPatro@users.noreply.github.com>
2020-10-01 16:08:25 +0530

    Added A new Category SearchEngine

---
+++ qual_id/categories/searchengine.py
@@ -0,0 +1,23 @@
+from ..category import Category
+
+
+class SearchEngine(Category):
+    def get_values(self):
+        return[
+            'Google',
+            'DuckDuckGo',
+            'Bing',
+            'Yahoo',
+            'Yandex',
+            'Swisscows',
+            'StartPage',
+            'Gibiru',
+            'OneSearch',
+            'Wiki.com',
+            'Boardreader',
+            'giveWater',
+            'Ekoru',
+            'Ecosia',
+            'Twitter',
+            'SlideShare'
+        ]
