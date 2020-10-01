ffb78a7d39b112d672568cd8a89c1e0958cb29fa
tanmay380 <38492979+tanmay380@users.noreply.github.com>
2020-10-01 09:15:59 +0530

    Create country.py

---
+++ qual_id/categories/country.py
@@ -0,0 +1,23 @@
+from ..category import Category
+
+class Country(Category):
+  def get_values(self):
+    return [
+        'India',
+        'China',
+        'Pakistan',
+        'Japan',
+        'Paris',
+        'iceberg',
+        'Cabo Verde,
+        'Cambodia',
+        'Cameroon',
+        'Canada',
+        'Chad',
+        'Chile',
+        'China',
+        'Colombia',
+        'Comoros',
+        'Italy',
+        'Jordan'
+]
