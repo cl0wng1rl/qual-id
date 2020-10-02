3538e3a2d8f3e273b697c7317b8a8cf89d5d33bc
Raahul Vignesh <raahulvignesh@Raahuls-MacBook-Pro.local>
2020-10-02 15:27:35 +0530

    Added vehicle category

---
+++ qual_id/categories/vehicle.py
@@ -0,0 +1,30 @@
+from ..category import Category
+
+class Vehicle(Category):
+  def get_values(self):
+    return [
+        'aeroplane',
+        'bus',
+        'boat',
+        'canoe',
+        'car',
+        'crane',
+        'cycle',
+        'ferry',
+        'forklift',
+        'helicopter',
+        'jet',
+        'lorry',
+        'motorbike',
+        'mountainbike',
+        'scooter',
+        'ship',
+        'skateboard',
+        'spaceship',
+        'submarine',
+        'tanker',
+        'train',
+        'truck',
+        'tractor',
+        'van'
+	  ]
