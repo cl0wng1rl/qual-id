f9f7f6e0fce6cd609db7db3d1f4d7c7cf1bf6649
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-03 01:05:24 +0100

    ALPHABETISE electronic category

--- qual_id/categories/electronic.py
+++ qual_id/categories/electronic.py
@@ -4,14 +4,14 @@ from ..category import Category
 class Electronic(Category):
     def get_values(self):
         return [
-            "laptop",
             "desktop",
-            "refrigerator",
-            "mobile",
-            "television",
-            "radio",
-            "trimmer",
-            "headphones",
-            "fan",
             "elevator",
+            "fan",
+            "headphones",
+            "laptop",
+            "mobile",
+            "radio",
+            "refrigerator",
+            "television",
+            "trimmer",
         ]
