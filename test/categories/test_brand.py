e99e3df2ba070d754988036e59b5289578d9aba1
jaqsparow <jaqsparow@yahoo.com>
2020-10-03 10:42:16 +0530

    update brand category

--- test/categories/test_brand.py
+++ test/categories/test_brand.py
@@ -4,12 +4,13 @@ from test.utils.category_helper import CategoryHelper
 
 
 class TestBrand(unittest.TestCase):
-  def setUp(self):
-    self.brand = Brand()
+    def setUp(self):
+        self.brand = Brand()
 
-  def test__get_values__is_valid(self):
-    self.assertTrue(CategoryHelper.get_values_is_valid(self.brand))
+    def test__get_values__is_valid(self):
+        error_message = CategoryHelper.get_values_error_message(self.brand)
+        self.assertTrue(error_message == "", error_message)
 
 
-if __name__ == '__main__':  # pragma: no cover
-  unittest.main()
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
