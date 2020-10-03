a0e7bfeca124cb051d3b9b9a20950f61bd6b14c6
aldinaufal21 <ry.prg75@gmail.com>
2020-10-03 16:30:39 +0700

    fix Test company category

--- test/categories/test_company.py
+++ test/categories/test_company.py
@@ -3,13 +3,14 @@ from qual_id.categories.company import Company
 from test.utils.category_helper import CategoryHelper
 
 
-class TestCompany(unittest.TestCase):
-  def setUp(self):
-    self.company = Company()
+class TestElectronic(unittest.TestCase):
+    def setUp(self):
+        self.company = Company()
 
-  def test__get_values__is_valid(self):
-        self.assertTrue(CategoryHelper.get_values_is_valid(self.company))
+    def test__get_values__is_valid(self):
+        error_message = CategoryHelper.get_values_error_message(self.company)
+        self.assertTrue(error_message == "", error_message)
 
 
-if __name__ == '__main__':  # pragma: no cover
-  unittest.main()
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
