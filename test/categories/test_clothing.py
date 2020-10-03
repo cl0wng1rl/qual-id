d0cfd3f1a07d18a9c33a4875da244a001563cb89
Gabriel <49308627+gabrielbarker@users.noreply.github.com>
2020-10-03 16:26:00 +0100

    Update test_clothing.py

--- test/categories/test_clothing.py
+++ test/categories/test_clothing.py
@@ -8,7 +8,8 @@ class TestClothing(unittest.TestCase):
         self.clothing = Clothing()
 
     def test__get_values__is_valid(self):
-        self.assertTrue(CategoryHelper.get_values_is_valid(self.clothing))
+        error_message = CategoryHelper.get_values_error_message(self.clothing)
+        self.assertTrue(error_message == "", error_message)
 
 
 if __name__ == '__main__':  # pragma: no cover
