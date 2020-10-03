b7d844b0f50b7c3a3cfb943d565f1cb3b041be51
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-03 01:10:33 +0100

    ALPHABETISE game category

--- qual_id/categories/game.py
+++ qual_id/categories/game.py
@@ -4,15 +4,15 @@ from ..category import Category
 class Game(Category):
     def get_values(self):
         return [
-            "basketball",
-            "cricket",
-            "hockey",
-            "chess",
-            "tennis",
             "badminton",
-            "football",
-            "wrestling",
-            "rugby",
             "baseball",
+            "basketball",
+            "chess",
+            "cricket",
             "cycling",
+            "football",
+            "hockey",
+            "rugby",
+            "tennis",
+            "wrestling",
         ]
