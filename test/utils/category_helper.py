de17299650a451af1df7129e6a393fdad7edd9fe
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-03 02:26:29 +0100

    TEST that get_values is in alphabetical order

--- test/utils/category_helper.py
+++ test/utils/category_helper.py
@@ -3,46 +3,52 @@ class CategoryHelper:
 
     @staticmethod
     def get_values_error_message(category):
-        returns_list_message = CategoryHelper.get_returns_list_error_message(category)
-        repeats_message = CategoryHelper.get_contains_repeats_message(category)
-        invalid_strings_message = CategoryHelper.get_contains_invalid_string_message(
-            category
-        )
-        messages = [returns_list_message, repeats_message, invalid_strings_message]
+        messages = [
+            CategoryHelper.__get_returns_list_error_message(category),
+            CategoryHelper.__get_contains_repeats_message(category),
+            CategoryHelper.__get_contains_invalid_string_message(category),
+            CategoryHelper.__get_alphabetical_message(category),
+        ]
+
         error_messages = [message for message in messages if message]
         return "\n".join(error_messages)
 
     @staticmethod
-    def get_returns_list_error_message(category):
-        returns_list = CategoryHelper.get_values_returns_a_list(category)
+    def __get_returns_list_error_message(category):
+        returns_list = CategoryHelper.__get_values_returns_a_list(category)
         return "" if returns_list else "should return non-empty list"
 
     @staticmethod
-    def get_values_returns_a_list(category):
+    def __get_values_returns_a_list(category):
         is_list = isinstance(category.get_values(), list)
         is_empty = len(category.get_values()) == 0
         return is_list and not is_empty
 
     @staticmethod
-    def get_contains_invalid_string_message(category):
+    def __get_contains_invalid_string_message(category):
         invalids = [
             value
             for value in category.get_values()
-            if CategoryHelper.string_contains_disallowed_character(value)
+            if CategoryHelper.__string_contains_disallowed_character(value)
         ]
-        return CategoryHelper.get_message("contains invalid strings: ", invalids)
+        return CategoryHelper.__get_message("contains invalid strings: ", invalids)
 
     @staticmethod
-    def get_contains_repeats_message(category):
+    def __get_contains_repeats_message(category):
         repeats = category.get_values()
         uniques = set(category.get_values())
         [repeats.remove(unique) for unique in uniques]
-        return CategoryHelper.get_message("contains repeats: ", repeats)
+        return CategoryHelper.__get_message("contains repeats: ", repeats)
 
     @staticmethod
-    def get_message(beginning, lst):
+    def __get_alphabetical_message(category):
+        message = "should be in alphabetical order"
+        return "" if sorted(category.get_values()) == category.get_values() else message
+
+    @staticmethod
+    def __get_message(beginning, lst):
         return (beginning + ", ".join(lst)) if len(lst) else ""
 
     @staticmethod
-    def string_contains_disallowed_character(str):
+    def __string_contains_disallowed_character(str):
         return any([c in str for c in CategoryHelper.__disallowed_characters])
