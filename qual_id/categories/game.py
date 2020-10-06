c12cc02272322a82c5e76dba8ea02b61940dcb77
Pravunath Singh <pravunathsingh@gmail.com>
2020-10-06 19:35:14 +0530

    Updated the game.py

--- qual_id/categories/game.py
+++ qual_id/categories/game.py
@@ -4,15 +4,37 @@ from ..category import Category
 class Game(Category):
     def get_values(self):
         return [
+            "archery"
+            "artistic swimming",
+            "athletics",
             "badminton",
             "baseball",
             "basketball",
+            "boxing",
+            "carrom"
             "chess",
             "cricket",
             "cycling",
+            "diving",
+            "equestrian jumping",
             "football",
+            "golf",
+            "gymnastics",
+            "handball",
             "hockey",
+            "judo",
+            "kabaddi",
+            "karate",
+            "rowing",
             "rugby",
+            "shooting",
+            "skateboarding",
+            "surfing",
+            "swimming",
+            "table tennis",
+            "taekwondo",
             "tennis",
+            "volleyball",
+            "weightlifting",
             "wrestling",
         ]
