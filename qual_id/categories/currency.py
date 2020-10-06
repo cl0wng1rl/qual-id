cd20a5abc73e430ad9c4ccab163c5333f4c2d263
Devan Aggarwal <42859208+devanaggarwal@users.noreply.github.com>
2020-10-07 00:51:18 +0530

    Create currency.py

---
+++ qual_id/categories/currency.py
@@ -0,0 +1,26 @@
+from ..category import Category
+
+
+class Currency(Category):
+  def get_values(self):
+    return [
+        'aud',
+        'cad',
+        'clp',
+        'cny',
+        'cop',
+        'czk',
+        'dkk',
+        'eur',
+        'gbp',
+        'hkd',
+        'idr',
+        'inr',
+        'jpy',
+        'nzd',
+        'rub',
+        'sar',
+        'sgd',
+        'usd',
+        'zar'
+    ]
