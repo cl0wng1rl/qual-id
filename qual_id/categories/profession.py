9f704800d43e2a50d025ba9aea4b7ead951151a2
jaqsparow <jaqsparow@yahoo.com>
2020-10-07 01:05:03 +0530

    add new category profession

---
+++ qual_id/categories/profession.py
@@ -0,0 +1,31 @@
+from ..category import Category
+
+
+class Profession(Category):
+    def get_values(self):
+        return[
+            "accountant",
+            "actor",
+            "architect",
+            "baker",
+            "butcher",
+            "chef",
+            "clerk",
+            "cook",
+            "dentist",
+            "designer",
+            "director",
+            "doctor",
+            "economist",
+            "editor",
+            "engineer",
+            "farmer",
+            "manager",
+            "Nurse",
+            "optician",
+            "painter",
+            "photographer",
+            "scientist",
+            "teacher",
+            "writer",
+        ]
