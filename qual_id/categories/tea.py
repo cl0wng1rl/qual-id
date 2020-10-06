12f6743223a3bafff1b631c3865e21ae53c28041
Gabriel Barker <gbarker@financialforce.com>
2020-10-07 00:41:51 +0100

    ADD tea category

---
+++ qual_id/categories/tea.py
@@ -0,0 +1,20 @@
+from ..category import Category
+
+
+class Tea(Category):
+    def get_values(self):
+        return [
+            "assam",
+            "ceylon",
+            "chai",
+            "chamomile",
+            "conguo",
+            "darjeeling",
+            "jasmine",
+            "kangra",
+            "matcha",
+            "nettle",
+            "oolong",
+            "peppermint",
+            "roobios",
+        ]
