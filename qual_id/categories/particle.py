f826bce3e02a863c7b78290003b1ce9f75119bb5
Gabriel Barker <gbarker@financialforce.com>
2020-10-07 01:07:19 +0100

    ADD particle category

---
+++ qual_id/categories/particle.py
@@ -0,0 +1,28 @@
+from ..category import Category
+
+
+class Particle(Category):
+    def get_values(self):
+        return [
+            "antimuon",
+            "antineutron",
+            "antiproton",
+            "antitau",
+            "atom",
+            "boson",
+            "electron",
+            "fermion",
+            "gluon",
+            "hadron",
+            "higgs",
+            "lepton",
+            "muon",
+            "neutrino",
+            "neutron",
+            "photon",
+            "positron",
+            "proton",
+            "quark",
+            "tachyon",
+            "tau",
+        ]
