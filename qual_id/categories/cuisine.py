e7b98cf28ec9b6ebc3c301e4cf76fe68891f2526
Pravunath Singh <pravunathsingh@gmail.com>
2020-10-08 13:30:47 +0530

    Added the Cuisine category

---
+++ qual_id/categories/cuisine.py
@@ -0,0 +1,49 @@
+from ..category import Category
+
+class Cuisine(Category):
+    def get_values(self):
+        return [
+            "afghani",
+            "algerian",
+            "american",
+            "belgian",
+            "brazilian",
+            "british",
+            "cajun",
+            "canadian",
+            "chinese",
+            "cuban",
+            "egyptian",
+            "french",
+            "german",
+            "greek",
+            "haitian",
+            "hawaiian",
+            "indian",
+            "irish",
+            "italian",
+            "japanese",
+            "jewish",
+            "kenyan",
+            "korean",
+            "latvian",
+            "libiyan",
+            "malyali",
+            "mexican",
+            "mormon",
+            "mughlai",
+            "nigerian",
+            "peruvian",
+            "polish",
+            "portuguese",
+            "russian",
+            "salvadorian"
+            "scottish",
+            "spanish",
+            "swedish",
+            "tahitian"
+            "thai",
+            "tibetian",
+            "turkish",
+            "welsh"
+        ]
