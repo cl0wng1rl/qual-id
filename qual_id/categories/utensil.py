6786fec252a820359457bc01df1bb9b2ef2e4cf5
jaqsparow <jaqsparow@yahoo.com>
2020-10-10 09:29:45 +0530

    update utensil category

--- qual_id/categories/utensil.py
+++ qual_id/categories/utensil.py
@@ -24,7 +24,6 @@ class Utensil(Category):
             "scoop",
             "spatula",
             "spoon",
-            "spoon",
             "strainer",
             "tongs",
             "tray"
