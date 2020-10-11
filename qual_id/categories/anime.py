dee678beb099725f134fc42ef5e23cfa95af0ee6
Kunal Kasodekar <kasodekarsunil@gmail.com>
2020-10-12 00:15:15 +0530

    Added anime category class. Added anime class test. Added anime category to pattern class

---
+++ qual_id/categories/anime.py
@@ -0,0 +1,33 @@
+from ..category import Category
+
+
+class Anime(Category):
+    def get_values(self):
+        return [
+            "yourname",
+            "fmab",
+            "erased",
+            "yourlieinapril",
+            "gardenofwords",
+            "naruto",
+            "mobpsycho",
+            "91days",
+            "attackontitan",
+            "oregairu",
+            "anohana",
+            "myheroacademia",
+            "haikyuu",
+            "tokyoghoul",
+            "hyouka",
+            "usagidrop",
+            "foodwars",
+            "koenokatachi",
+            "relife",
+            "onepiece",
+            "bleach",
+            "konosuba",
+            "danmachi",
+            "wotakoi",
+            "tenkinoko",
+        ]
+   
