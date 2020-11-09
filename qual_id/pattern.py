0892193db5c455b93a5e2ae70db1c69a4594967d
Gabriel Barker <gabrielbarker@gmail.com>
2020-11-09 02:53:45 +0000

    REPLACE static map with category_map in pattern

--- qual_id/pattern.py
+++ qual_id/pattern.py
@@ -1,79 +1,27 @@
 from random import choice
-from .categories import *
+from qual_id.category_map import CategoryMap
 
 
 class Pattern:
     __random_key = "random"
 
-    __category_map = {
-        "adjective": Adjective(),
-        "animal": Animal(),
-        "author": Author(),
-        "bird": Bird(),
-        "book": Book(),
-        "brand": Brand(),
-        "capital": Capital(),
-        "city": City(),
-        "clothing": Clothing(),
-        "color": Color(),
-        "constellation": Constellation(),
-        "country": Country(),
-        "cuisine": Cuisine(),
-        "currency": Currency(),
-        "drink": Drink(),
-        "electronic": Electronic(),
-        "element": Element(),
-        "emotion": Emotion(),
-        "festivals": Festivals(),
-        "film": Film(),
-        "food": Food(),
-        "fruit": Fruit(),
-        "geography": Geography(),
-        "id": RandomId(),
-        "instrument": Instrument(),
-        "language": Language(),
-        "music": Music(),
-        "operatingsystem": OperatingSystem(),
-        "particle": Particle(),
-        "planet": Planet(),
-        "pokemon": Pokemon(),
-        "profession": Profession(),
-        "programminglanguage": ProgrammingLanguage(),
-        "scientist": Scientist(),
-        "searchengine": SearchEngine(),
-        "shape": Shape(),
-        "sport": Sport(),
-        "state": State(),
-        "tea": Tea(),
-        "tool": Tool(),
-        "utensil": Utensil(),
-        "vehicle": Vehicle(),
-        "wine": Wine(),
-    }
-
     @staticmethod
     def get_category_options():
-        return list(Pattern.__category_map.keys())
+        return CategoryMap.all()
 
-    def __init__(self, pattern_string):
+    def __init__(self, pattern_string, category_map=CategoryMap([])):
         categories_array = [p for p in pattern_string.split("-") if p != ""]
         self.__categories = self.__replace_randoms(categories_array)
+        self.__category_map = category_map
 
     def has_acceptable_categories_length(self):
-        return self.__categories_length_is_acceptable()
+        return 0 < len(self.__categories) < 6
 
     def get_nonexistent_categories(self):
-        return [
-            category
-            for category in self.__categories
-            if category not in Pattern.__category_map
-        ]
+        return CategoryMap.invalid(self.__categories)
 
     def get_categories(self):
-        return [Pattern.__category_map[category] for category in self.__categories]
-
-    def __categories_length_is_acceptable(self):
-        return 0 < len(self.__categories) < 6
+        return [self.__category_map.get(category) for category in self.__categories]
 
     @staticmethod
     def __random_category():
