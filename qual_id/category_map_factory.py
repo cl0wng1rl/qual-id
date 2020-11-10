f619ba5c7c8f6d54e7da93b3600e35953d287b71
Gabriel Barker <gabrielbarker@gmail.com>
2020-11-10 02:37:31 +0000

    ADD category_map_factory class

---
+++ qual_id/category_map_factory.py
@@ -0,0 +1,54 @@
+from qual_id.category_map import CategoryMap
+
+class CategoryMapFactory:
+  @staticmethod
+    def all():
+      return CategoryMap()
+  
+  @staticmethod
+    def minimal():
+      categories = [
+        "adjective",
+        "animal",
+        "bird",
+        "color",
+        "food",
+        "fruit",
+        "geography",
+        "instrument",
+        "planet",
+        "tea",
+        "vehicle",
+      ]
+      return CategoryMap(categories)
+  
+  @staticmethod
+    def neutral():
+      categories = [
+        "adjective",
+        "animal",
+        "bird",
+        "capital",
+        "city",
+        "clothing",
+        "color",
+        "constellation",
+        "country",
+        "currency",
+        "drink",
+        "element",
+        "emotion",
+        "food",
+        "fruit",
+        "geography",
+        "instrument",
+        "particle",
+        "planet",
+        "state",
+        "tea",
+        "tool",
+        "utensil",
+        "vehicle",
+        "wine",
+      ]
+      return CategoryMap(categories)
