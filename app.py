c5677312721f02c970388b7959279961e2adc49c
Gabriel Barker <gabrielbarker@gmail.com>
2020-11-11 21:27:29 +0000

    USE response class in app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template, make_response
+from qual_id.response import Response
 from qual_id.validator import Validator
 from qual_id.pattern import Pattern
 from qual_id.category_map import CategoryMap
@@ -10,9 +11,7 @@ app = Flask(__name__)
 
 @app.route("/get/", methods=["GET"])
 def get_response():
-    response_obj = get_response_obj(request.args)
-    response_format = request.args.get("format", "json")
-    return get_response_in_format(response_obj, response_format)
+    return make_response(Response(request.args).get_response_obj())
 
 
 @app.route("/categories/", methods=["GET"])
@@ -25,49 +24,19 @@ def categories_response():
 
 @app.route("/badge-endpoint/", methods=["GET"])
 def badge_endpoint_response():
-    example = get_qual_ids("fruit-geography", "all", 1)[0]
-    response_obj = {
-        "schemaVersion": 1,
-        "label": "Qual ID",
-        "message": example,
-        "color": f"hsl({random.randint(0,359)}, 100%, 50%)",
-    }
-    response = make_response(response_obj)
+    qual_id = Response({"pattern": "fruit-geography"}).get_qual_ids()[0]
+    response = make_response(get_badge_response_object(qual_id))
     response.headers["Cache-Control"] = "no-cache, no-store"
     return response
 
 
-def get_response_obj(args):
-    pattern_string = args.get("pattern", "")
-    collection_string = args.get("collection", "all")
-    number = int(args.get("number", 1))
-
-    response_obj = {}
-    validator = Validator(pattern_string, collection_string)
-    error = validator.error()
-    if error:
-        response_obj["error"] = error
-    else:
-        category_map = CategoryMapFactory.get(collection_string)
-        pattern = Pattern(pattern_string, category_map)
-        response_obj["data"] = get_qual_ids(pattern_string, collection_string, number)
-    return response_obj
-
-
-def get_qual_ids(pattern_string, collection_string, number):
-    category_map = CategoryMapFactory.get(collection_string)
-    pattern = Pattern(pattern_string, category_map)
-    return [get_qual_id(pattern) for _ in range(number)]
-
-
-def get_qual_id(pattern):
-    return "-".join([category.random() for category in pattern.get_categories()])
-
-
-def get_response_in_format(response_obj, response_format):
-    if response_format == "csv":
-        return make_response(",".join(response_obj["data"]) or response_obj["error"])
-    return make_response(response_obj)
+def get_badge_response_object(message):
+    return {
+        "schemaVersion": 1,
+        "label": "Qual ID",
+        "message": message,
+        "color": f"hsl({random.randint(0,359)}, 100%, 50%)",
+    }
 
 
 @app.route("/")
