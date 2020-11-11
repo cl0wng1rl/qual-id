3698ed64a88e3e70e7b9e82cabd4faa547399b13
Gabriel Barker <gabrielbarker@gmail.com>
2020-11-11 02:11:11 +0000

    ADD more tests for category map factory

--- test/test_category_map_factory.py
+++ test/test_category_map_factory.py
@@ -4,14 +4,23 @@ from qual_id.category_map_factory import CategoryMapFactory
 
 
 class TestCategoryMapFactory(unittest.TestCase):
-    def test__all__returns_category_map(self):
+    def test__get__empty_string__returns_category(self):
         self.assertIsInstance(CategoryMapFactory.get(""), CategoryMap)
 
-    def test__minimal__returns_category(self):
+    def test__get__minimal__returns_category(self):
         self.assertIsInstance(CategoryMapFactory.get("minimal"), CategoryMap)
 
-    def test__neutral__returns_category(self):
+    def test__get__neutral__returns_category(self):
         self.assertIsInstance(CategoryMapFactory.get("neutral"), CategoryMap)
+
+    def test__has__invalid_string__returns_false(self):
+        self.assertFalse(CategoryMapFactory.has("$$$"))
+
+    def test__has__minimal__returns_false(self):
+        self.assertTrue(CategoryMapFactory.has("minimal"))
+
+    def test__has__neutral__returns_false(self):
+        self.assertTrue(CategoryMapFactory.has("neutral"))
 
 
 if __name__ == "__main__":  # pragma: no cover
