028729e91ae74ab27f611f963a5bf92a3eab16bf
Gabriel Barker <gabrielbarker@gmail.com>
2020-11-12 21:00:45 +0000

    ADD gem category

---
+++ qual_id/categories/gem.py
@@ -0,0 +1,26 @@
+from ..category import Category
+
+
+class Gem(Category):
+    def get_values(self):
+        return [
+            "amazonite",
+            "amethyst",
+            "aquamarine",
+            "bloodstone",
+            "diamond",
+            "emerald",
+            "garnet",
+            "jade",
+            "jet",
+            "moonstone",
+            "onyx",
+            "opal",
+            "quartz",
+            "ruby",
+            "sapphire",
+            "sunstone",
+            "topaz",
+            "turquoise",
+            "zircon",
+        ]
