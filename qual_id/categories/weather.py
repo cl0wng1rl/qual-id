22d2f673bb08253783c4e76b785e3cd1423c205a
Gabriel Barker <gabrielbarker@gmail.com>
2020-11-12 21:08:04 +0000

    ADD weather category

---
+++ qual_id/categories/weather.py
@@ -0,0 +1,33 @@
+from ..category import Category
+
+
+class Weather(Category):
+    def get_values(self):
+        return [
+            "breeze",
+            "cloud",
+            "cyclone",
+            "drizzle",
+            "fog",
+            "frost",
+            "gale",
+            "gust",
+            "hail",
+            "heatwave",
+            "hurricane",
+            "lightning",
+            "mist",
+            "rain",
+            "sandstorm",
+            "shower",
+            "sleet",
+            "snow",
+            "storm",
+            "sunshine",
+            "thunder",
+            "tornado",
+            "tsunami",
+            "typhoon",
+            "whirlwind",
+            "wind",
+        ]
