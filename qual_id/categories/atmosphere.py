f4db32b01efbd205e40ba3407eb3d55689388f6e
Gabriel Barker <gabrielbarker@gmail.com>
2020-11-12 21:15:02 +0000

    ADD atmosphere category

---
+++ qual_id/categories/atmosphere.py
@@ -0,0 +1,39 @@
+from ..category import Category
+
+
+class Atmosphere(Category):
+    def get_values(self):
+        return [
+            "arctic",
+            "baking",
+            "balmy",
+            "blistering",
+            "blustery",
+            "breezy",
+            "clear",
+            "cloudy",
+            "cold",
+            "damp",
+            "drizzly",
+            "dry",
+            "foggy",
+            "freezing",
+            "hazy",
+            "hot",
+            "humid",
+            "icy",
+            "mild",
+            "misty",
+            "overcast",
+            "rainy",
+            "scorching",
+            "showery",
+            "snowy",
+            "stormy",
+            "sunny",
+            "thundery",
+            "tropical",
+            "wet",
+            "windy",
+            "wintry",
+        ]
