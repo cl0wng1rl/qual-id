204de9fd6cb8ed167b33819713218f37b54f54b9
Gabriel Barker <gabrielbarker@gmail.com>
2020-11-12 01:59:02 +0000

    ADD cake category

---
+++ qual_id/categories/cake.py
@@ -0,0 +1,35 @@
+from ..category import Category
+
+
+class Cake(Category):
+    def get_values(self):
+        return [
+            "baklava",
+            "biscuit",
+            "blondie",
+            "brownie",
+            "bun",
+            "cheescake",
+            "crumpet",
+            "cupcake",
+            "donut",
+            "dumpling",
+            "eclair",
+            "flapjack",
+            "frangipane",
+            "fruitcake",
+            "gateau",
+            "gingerbread",
+            "ladyfinger",
+            "meringue",
+            "muffin",
+            "pancake",
+            "panettone",
+            "profiterole",
+            "scone",
+            "tart",
+            "teacake",
+            "torte",
+            "trifle",
+            "turnover",
+        ]
