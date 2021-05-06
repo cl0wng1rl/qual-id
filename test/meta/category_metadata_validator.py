f8bd518f051226c7e63d7d68de7b6129ff1bd96b
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-06 17:46:57 +0100

    ADD suite to test the metadata of all categories
    This separation of tests is because these tests don't test functionality

---
+++ test/meta/category_metadata_validator.py
@@ -0,0 +1,70 @@
+class CategoryMetadataValidator:
+    __disallowed_characters = [" ", "-", "_", "&"]
+
+
+    @staticmethod
+    def get_values_error_message(category):
+        messages = [
+            CategoryMetadataValidator.__get_returns_list_error_message(category),
+            CategoryMetadataValidator.__get_contains_repeats_message(category),
+            CategoryMetadataValidator.__get_contains_invalid_string_message(category),
+            CategoryMetadataValidator.__get_alphabetical_message(category),
+            CategoryMetadataValidator.__get_contains_uppercase_string_message(category),
+        ]
+
+        error_messages = [message for message in messages if message]
+        return "\n".join(error_messages)
+
+    @staticmethod
+    def __get_returns_list_error_message(category):
+        returns_list = CategoryMetadataValidator.__get_values_returns_a_list(category)
+        return "" if returns_list else "should return non-empty list"
+
+    @staticmethod
+    def __get_values_returns_a_list(category):
+        is_list = isinstance(category._values, list)
+        is_empty = len(category._values) == 0
+        return is_list and not is_empty
+
+    @staticmethod
+    def __get_contains_repeats_message(category):
+        repeats = list(category._values)
+        uniques = set(repeats)
+        [repeats.remove(unique) for unique in uniques]
+        return CategoryMetadataValidator.__get_message("contains repeats: ", repeats)
+
+    @staticmethod
+    def __get_contains_invalid_string_message(category):
+        invalids = [
+            value
+            for value in category._values
+            if CategoryMetadataValidator.__string_contains_disallowed_character(value)
+        ]
+        return CategoryMetadataValidator.__get_message("contains invalid strings: ", invalids)
+
+    @staticmethod
+    def __get_alphabetical_message(category):
+        message = "should be in alphabetical order"
+        values = category._values
+        return "" if sorted(values) == values else message
+
+    @staticmethod
+    def __get_contains_uppercase_string_message(category):
+        invalids = [
+            value
+            for value in category._values
+            if CategoryMetadataValidator.__string_contains_uppercase_character(value)
+        ]
+        return CategoryMetadataValidator.__get_message("contains uppercase strings: ", invalids)
+
+    @staticmethod
+    def __get_message(beginning, lst):
+        return (beginning + ", ".join(lst)) if len(lst) else ""
+
+    @staticmethod
+    def __string_contains_disallowed_character(str):
+        return any([c in str for c in CategoryMetadataValidator.__disallowed_characters])
+
+    @staticmethod
+    def __string_contains_uppercase_character(str):
+        return any([c for c in list(str) if c.isupper()])
---
+++ test/meta/test_categories.py
@@ -0,0 +1,80 @@
+import unittest
+from qual_id.categories import *
+from test.meta.category_metadata_validator import CategoryMetadataValidator
+
+
+class TestAnimal(unittest.TestCase):
+    def setUp(self):
+        self.categories = TestAnimal._get_all_categories()
+
+    def test__all_categories__values_are_valid(self):
+        [self.validate_values_are_valid(category) for category in self.categories]
+
+    def validate_values_are_valid(self, category):
+        error_message = CategoryMetadataValidator.get_values_error_message(category)
+        self.assertTrue(error_message == "", TestAnimal._user_error_message(category, error_message))
+
+    @staticmethod
+    def _user_error_message(category, error_message):
+      return "\n" + category._name + ": \n" + error_message
+      
+    @staticmethod
+    def _get_all_categories():
+      return [
+        Adjective,
+        Animal,
+        Atmosphere,
+        Author,
+        Bird,
+        Book,
+        Brand,
+        Bread,
+        Cake,
+        Capital,
+        City,
+        Clothing,
+        Color,
+        Constellation,
+        Country,
+        Cuisine,
+        Currency,
+        Dog,
+        Drink,
+        Electronic,
+        Element,
+        Emotion,
+        Festivals,
+        Film,
+        Fish,
+        Flower,
+        Food,
+        Fruit,
+        Gem,
+        Geography,
+        Instrument,
+        Language,
+        Music,
+        OperatingSystem,
+        Particle,
+        Planet,
+        Pokemon,
+        Profession,
+        ProgrammingLanguage,
+        RandomId,
+        Scientist,
+        SearchEngine,
+        Shape,
+        Spice,
+        Sport,
+        State,
+        Tea,
+        Tool,
+        Utensil,
+        Vegetable,
+        Vehicle,
+        Weather,
+        Wine,
+      ]
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
