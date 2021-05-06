00d84bacb0a972a598d1f7093a836367d1050fa5
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-06 10:20:08 +0100

    ADD bread category

---
+++ qual_id/categories/bread.py
@@ -0,0 +1,30 @@
+from ..category import Category
+
+
+class Bread(Category):
+    def get_values(self):
+        return [
+            "bagel",
+            "baguette",
+            "brioche",
+            "bun",
+            "chapati",
+            "ciabatta",
+            "cornbread",
+            "croissant",
+            "focaccia",
+            "granary",
+            "matzo",
+            "muffin",
+            "naan",
+            "paratha",
+            "pitta",
+            "poppadom",
+            "pumpernickel",
+            "roll",
+            "roti",
+            "rye",
+            "sourdough",
+            "tortilla",
+            "wholemeal"
+        ]
