a1de8be7514751d8ed81b8c13cfd832194ffa985
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-06 10:20:16 +0100

    ADD dog category

---
+++ qual_id/categories/dog.py
@@ -0,0 +1,37 @@
+from ..category import Category
+
+
+class Dog(Category):
+    def get_values(self):
+        return [
+            "alsacaian",
+            "beagle",
+            "bloodhound",
+            "boxer",
+            "bulldog",
+            "chihuahua",
+            "collie",
+            "corgi",
+            "dachshund",
+            "dalmatian",
+            "doberman",
+            "foxhound",
+            "greyhound",
+            "husky",
+            "labradoodle",
+            "labrador",
+            "maltese",
+            "mastiff",
+            "otterhound",
+            "pomeranian",
+            "poodle",
+            "pug",
+            "retriever",
+            "rottweiler",
+            "schnauzer",
+            "spaniel",
+            "terrier",
+            "whippet",
+            "wolfhound",
+        ]
+
