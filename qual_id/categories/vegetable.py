c8e0244733211b540b0b0b0eb99efd93153a3860
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-06 10:20:27 +0100

    ADD vegetable category

---
+++ qual_id/categories/vegetable.py
@@ -0,0 +1,42 @@
+from ..category import Category
+
+
+class Vegetable(Category):
+    def get_values(self):
+        return [
+            "ackee",
+            "asparagus",
+            "aubergine",
+            "beetroot",
+            "broccoli",
+            "cabbage",
+            "carrot",
+            "cauliflower",
+            "celeriac",
+            "celery",
+            "chard",
+            "chive",
+            "corn",
+            "courgette",
+            "cress",
+            "cucumber",
+            "eggplant",
+            "fennel",
+            "gherkin",
+            "horseradish",
+            "kale",
+            "leek",
+            "lettuce",
+            "marrow",
+            "onion",
+            "parsnip",
+            "potato",
+            "radish",
+            "shallot",
+            "spinach",
+            "squash",
+            "swede",
+            "turnip",
+            "yam",
+            "zucchini"
+        ]
