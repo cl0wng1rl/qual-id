79e9a0f16574ed64eb1104e37930591e9348ec70
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-07 09:30:43 +0100

    TEST `Category` and `RandomId` through their API

--- test/unit/test_category.py
+++ test/unit/test_category.py
@@ -4,19 +4,14 @@ import random
 
 
 class TestCategory(unittest.TestCase):
-    def setUp(self):
-        self.category = Category()
 
-    def test__get_values__returns_list_with_empty_string(self):
-        self.assertEqual(self.category._values, [""])
+    def test__get_name__returns_category(self):
+        self.assertEqual(Category.get_name(), "category")
 
     def test__get_random_choice__mock_random_choice__returns_empty_string(self):
         random.seed(0)
-        fixed_choice = self.__get_random_choice_from_values()
-        self.assertEqual(self.category.random(), fixed_choice)
-
-    def __get_random_choice_from_values(self):
-        return random.choice(self.category._values)
+        fixed_choice = random.choice(Category._values)
+        self.assertEqual(Category.random(), fixed_choice)
 
 
 if __name__ == "__main__":  # pragma: no cover
---
+++ test/unit/test_category_randomid.py
@@ -0,0 +1,20 @@
+import unittest
+from qual_id.categories.randomid import RandomId
+from test.unit.utils.category_helper import CategoryHelper
+import random
+
+
+class TestRandomId(unittest.TestCase):
+    def setUp(self):
+        random.seed(1)
+
+    def test__get_values__is_random_id(self):
+        randomId = RandomId()
+        result = randomId.random()
+        expected_id = "ieqh524yng"
+
+        self.assertEqual(expected_id, result)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
