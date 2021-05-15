6fa2b2767f82bfb67ad56477267163d2189bd298
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-15 16:34:26 +0100

    TEST the api of the `Collection` class

--- test/unit/test_collection.py
+++ test/unit/test_collection.py
@@ -1,19 +1,23 @@
 import unittest
+from qual_id.categories.fruit import Fruit
+from qual_id.categories.fish import Fish
 from qual_id.category import Category
 from qual_id.collections.all import All
+import random
 
 
 class TestCollection(unittest.TestCase):
     VALID_KEY = "fruit"
     SECOND_VALID_KEY = "geography"
-    THIRD_VALID_KEY = "bird"
+    THIRD_VALID_KEY = "fish"
     INVALID_KEY = "$$$"
+    ALL_COLLECTION_NAME = "all"
 
     def setUp(self):
         self.collection = All()
 
-    def test__get__valid_key__returns_category(self):
-        self.assertIsInstance(self.collection.get(self.VALID_KEY)(), Category)
+    def test__get__valid_key__returns_correct_category(self):
+        self.assertIsInstance(self.collection.get(self.VALID_KEY)(), Fruit)
         
     def test__invalid__with_valid_keys__returns_empty_list(self):
         keys_to_validate = [self.VALID_KEY, self.SECOND_VALID_KEY]
@@ -23,6 +27,13 @@ class TestCollection(unittest.TestCase):
         keys_to_validate = [self.VALID_KEY, self.INVALID_KEY]
         result = self.collection.invalid(keys_to_validate)
         self.assertEqual(result, [self.INVALID_KEY])
+    
+    def test__random__mock_random_choice__returns_correct_category(self):
+        random.seed(0)
+        self.assertIsInstance(self.collection.random()(), Fish)
+
+    def test__name__all_collection__returns_name_all(self):
+        self.assertEqual(self.collection.name(), self.ALL_COLLECTION_NAME)
 
 
 if __name__ == "__main__":  # pragma: no cover
