fb9a3eded2b008cf95a1fe53c7e961b532c35751
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-16 02:15:28 +0100

    REFACTOR `Validator` class

--- qual_id/validator.py
+++ qual_id/validator.py
@@ -1,15 +1,34 @@
-from qual_id.pattern import Pattern
-from qual_id.collection_factory import CollectionFactory
+from qual_id.validators import *
 
 
 class Validator:
-    def __init__(self, pattern_string, collection_string):
-        self.__pattern_string = pattern_string
-        self.__collection_string = collection_string
-        self.__pattern = None
+    def __init__(self, config):
+        self._validators = Validator._get_validators(config)
+        self._is_valid = None
+        self._error_message = None
 
-    def error(self):
-        if not CollectionFactory.has(self.__collection_string):
-            return "invalid collection: %s" % (self.__collection_string)
-        collection = CollectionFactory.get(self.__collection_string)
-        return Pattern(self.__pattern_string, collection).error()
+    def validate(self):
+        index = 0
+        while (index < len(self._validators)) and (self._is_valid != False):
+            self._validate(self._validators[index])
+            index += 1
+
+    def is_valid(self):
+        return self._is_valid
+
+    def error_message(self):
+        return self._error_message
+
+    def _validate(self, validator):
+        validator.validate()
+        self._error_message = validator.error_message()
+        self._is_valid = validator.is_valid()
+
+    @staticmethod
+    def _get_validators(config):
+        return [
+            CollectionValidator(config.get_collection()),
+            PatternValidator(config.get_collection(), config.get_categories()),
+            FormatValidator(config.get_format()),
+            NumberValidator(config.get_number()),
+        ]
