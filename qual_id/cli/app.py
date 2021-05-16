7979c189c9f92aad69e8e9c87f1ddf31f47d4256
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-17 00:22:07 +0100

    SEPARATE validation from parsing in cli module

--- qual_id/cli/app.py
+++ qual_id/cli/app.py
@@ -1,10 +1,45 @@
 from qual_id.response import Response
 from qual_id.cli.parser import Parser
+from qual_id.cli.validator import Validator
 
 
 class App:
     @staticmethod
     def run(args):
+        App._validate_arguments(args)
+        return App._get_response(args)
+    
+    @staticmethod
+    def _validate_arguments(args):
+        validator = Validator(args)
+        if validator.is_help():
+            App._print_help()
+        if not validator.is_valid():
+            App._print_error(validator)
+
+    @staticmethod
+    def _get_response(args):
         parser = Parser(args)
         config = parser.parse()
         return Response(config).get_response_obj()
+
+    @staticmethod
+    def _print_help():
+        print(" ")
+        print("Qual ID - get qualitative IDs")
+        print(" ")
+        print("qual-id [options]")
+        print(" ")
+        print("options:")
+        print("-h, --help                show brief help")
+        print("-p, --pattern             specify the pattern of the qual IDs")
+        print("-c, --collection          specify which collection to use")
+        print("-n, --number              specify how many qual IDs to receive")
+        print("-f, --format              specify the format of the qual IDs")
+        print(" ")
+        exit()
+    
+    @staticmethod
+    def _print_error(validator):
+        print(validator.error_message())
+        exit()
--- qual_id/cli/parser.py
+++ qual_id/cli/parser.py
@@ -3,51 +3,21 @@ from qual_id.cli.flag import Flag
 class Parser:
     def __init__(self, arguments):
         self._arguments = arguments
-        self._shift_arguments()
         self._config = {"format": "csv"}
+        self._shift_arguments()
 
     def parse(self):
-        self._check_for_help()
         while len(self._arguments):
             self._extract_parameter(self._get_next_flag())
         return self._config
 
-    def _check_for_help(self):
-        if any(map(lambda arg: Flag.HELP.value.equals(arg), self._arguments)):
-            self._print_help()
-            exit()
-    
     def _get_next_flag(self):
-        flag = next((flag for flag in Flag if flag.value.equals(self._arguments[0])), None)
-        if not flag:
-            print("invalid parameter: " + self._arguments[0])
-            exit()
-        return flag
-
-    def _check_for_remaining_args(self, flag):
-        if not len(self._arguments):
-            print("no {0} specified".format(flag.value.name()))
-            exit()
-    
-    def _shift_arguments(self):
-        self._arguments = self._arguments[1:]
+        return next((flag for flag in Flag if flag.value.equals(self._arguments[0])), None)
 
     def _extract_parameter(self, flag):
         self._shift_arguments()
-        self._check_for_remaining_args(flag)
         self._config[flag.value.name()] = self._arguments[0]
         self._shift_arguments()
 
-    def _print_help(self):
-        print(" ")
-        print("Qual ID - get qualitative IDs")
-        print(" ")
-        print("qual-id [options]")
-        print(" ")
-        print("options:")
-        print("-h, --help                show brief help")
-        print("-p, --pattern             specify the pattern of the qual IDs")
-        print("-c, --collection          specify which collection to use")
-        print("-n, --number              specify how many qual IDs to receive")
-        print("-f, --format              specify the format of the qual IDs")
-        print(" ")
+    def _shift_arguments(self):
+        self._arguments = self._arguments[1:]
