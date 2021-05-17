9c97839f9ee712ff91a4045bb6e878d058dc0b3c
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-17 23:05:10 +0100

    TEST `info` method on `Category` class

--- test/unit/categories/test_category.py
+++ test/unit/categories/test_category.py
@@ -4,15 +4,23 @@ import random
 
 
 class TestCategory(unittest.TestCase):
+    CATEGORY_NAME = "category"
+    CATEGORY_VALUES = ["valuea", "valueb", "valuec"]
+    
+    def test__name__correct_name(self):
+        self.assertEqual(MockCategory.name(), TestCategory.CATEGORY_NAME)
 
-    def test__name__returns_category(self):
-        self.assertEqual(Category.name(), "")
-
-    def test__random__mock_random_choice__returns_empty_string(self):
+    def test__random__mock_random_choice__correct_value(self):
         random.seed(0)
-        fixed_choice = random.choice(Category._values)
-        self.assertEqual(Category.random(), fixed_choice)
+        fixed_choice = TestCategory.CATEGORY_VALUES[1]
+        self.assertEqual(MockCategory.random(), fixed_choice)
 
+    def test__info__correct_values(self):
+        self.assertListEqual(MockCategory.info(), TestCategory.CATEGORY_VALUES)
+
+class MockCategory(Category):
+    _name = TestCategory.CATEGORY_NAME
+    _values = TestCategory.CATEGORY_VALUES
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
