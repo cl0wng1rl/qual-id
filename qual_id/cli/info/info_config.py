df027d0af51724febae4fa4f5fb4e357c46b3d3a
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-18 09:11:49 +0100

    ADD `InfoConfig` class to handle info messages

---
+++ qual_id/cli/info/info_config.py
@@ -0,0 +1,13 @@
+class InfoConfig:
+    def __init__(self, parameter, value, info):
+        self._type = parameter.value.name()
+        self._value = value
+        self._info = info
+    
+    def message(self):
+      if self._value == None:
+        return "Options for '{0}' parameter: \n{1}".format(self._type, self._info_string())
+      return "Values for the {0}, '{1}': \n{2}".format(self._type, self._value, self._info_string())
+
+    def _info_string(self):
+        return ", ".join(self._info)
