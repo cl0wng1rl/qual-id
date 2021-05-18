35af684e77931c5d56eaf8e5eb1be91901c517ea
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-18 09:12:16 +0100

    TEST the `InfoConfig` class

---
+++ test/unit/cli/info/test_info_config.py
@@ -0,0 +1,25 @@
+import unittest
+from qual_id.cli.info.flag import Flag
+from qual_id.cli.info.info_config import InfoConfig
+
+
+class TestInfoConfig(unittest.TestCase):
+    TYPE = Flag.CATEGORY
+    TYPE_NAME = Flag.CATEGORY.value.name()
+    VALUE = "value"
+    INFO = ["value1", "value2", "value3"]
+    INFO_STRING = ", ".join(INFO)
+
+    def test__value__correct_message(self):
+        config = InfoConfig(self.TYPE, self.VALUE, self.INFO)
+        excepted_message = "Values for the {0}, '{1}': \n{2}".format(self.TYPE_NAME, self.VALUE, self.INFO_STRING)
+        self.assertEqual(excepted_message, config.message())
+
+    def test__no_value__correct_message(self):
+        config = InfoConfig(self.TYPE, None, self.INFO)
+        excepted_message = "Options for '{0}' parameter: \n{1}".format(self.TYPE_NAME, self.INFO_STRING)
+        self.assertEqual(excepted_message, config.message())
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
