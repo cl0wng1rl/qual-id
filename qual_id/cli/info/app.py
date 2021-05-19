8e5ce83ffe00ff34c1f171b20fb53b14bc7a8f2b
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-20 00:57:33 +0100

    ADD `Helper` logic to `info` app

--- qual_id/cli/info/app.py
+++ qual_id/cli/info/app.py
@@ -1,10 +1,16 @@
+from .helper import Helper
 from .validator import Validator
 from .parser import Parser
 
 class App:
     @staticmethod
     def run(args):
+        helper = Helper(args)
+        if helper.is_help():
+            return helper.help_messge()
+
         validator = Validator(args)
         if validator.is_valid():
             return Parser().parse(args).message()
+
         return validator.error_message()
