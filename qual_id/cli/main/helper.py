57e9b6f57a3e6c155974ddfbaefbde3b8b48cb13
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-20 00:57:16 +0100

    ADD `Helper` class to `info` module

---
+++ qual_id/cli/main/helper.py
@@ -0,0 +1,17 @@
+from qual_id.cli.helper import Helper as IHelper
+
+class Helper(IHelper):
+    HELP_MESSAGE_LINES = [
+        " ",
+        "Qual ID - get qualitative IDs",
+        " ",
+        "qual-id [options]",
+        " ",
+        "options:",
+        "-h, --help                show brief help",
+        "-p, --pattern             specify the pattern of the qual IDs",
+        "-c, --collection          specify which collection to use",
+        "-n, --number              specify how many qual IDs to receive",
+        "-f, --format              specify the format of the qual IDs",
+        " ",
+    ]
