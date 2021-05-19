15688c17e2ecb31dd4bb415249deee40f187b910
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-20 00:59:48 +0100

    ADD `Helper` logic to `main` app

--- qual_id/cli/main/app.py
+++ qual_id/cli/main/app.py
@@ -1,45 +1,18 @@
 from qual_id.response import Response
-from qual_id.cli.main.parser import Parser
+from qual_id.cli.main.helper import Helper
 from qual_id.cli.main.validator import Validator
+from qual_id.cli.main.parser import Parser
 
 
 class App:
     @staticmethod
     def run(args):
-        App._validate_arguments(args)
-        return App._get_response(args)
-    
-    @staticmethod
-    def _validate_arguments(args):
+        helper = Helper(args)
+        if helper.is_help():
+            return helper.help_messge()
+
         validator = Validator(args)
-        if validator.is_help():
-            App._print_help()
-        if not validator.is_valid():
-            App._print_error(validator)
+        if validator.is_valid():
+            return Response(Parser(args).parse()).get_response_obj()
 
-    @staticmethod
-    def _get_response(args):
-        parser = Parser(args)
-        config = parser.parse()
-        return Response(config).get_response_obj()
-
-    @staticmethod
-    def _print_help():
-        print(" ")
-        print("Qual ID - get qualitative IDs")
-        print(" ")
-        print("qual-id [options]")
-        print(" ")
-        print("options:")
-        print("-h, --help                show brief help")
-        print("-p, --pattern             specify the pattern of the qual IDs")
-        print("-c, --collection          specify which collection to use")
-        print("-n, --number              specify how many qual IDs to receive")
-        print("-f, --format              specify the format of the qual IDs")
-        print(" ")
-        exit()
-    
-    @staticmethod
-    def _print_error(validator):
-        print(validator.error_message())
-        exit()
+        return validator.error_message()
