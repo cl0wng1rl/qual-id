101a0466d48f3db909bf43f1d938bf9486685b9c
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-20 01:10:00 +0100

    TEST `Helper` base class
    Testing implementations would just assert that the text hasn't changed

---
+++ test/unit/cli/test_helper.py
@@ -0,0 +1,33 @@
+import unittest
+from qual_id.cli.helper import Helper
+
+
+
+class TestHelper(unittest.TestCase):
+    HELP_LONG = "--help"
+    HELP_SHORT = "-h"
+    HELP_MESSAGE_LINES = ["line1", "line2", "line3"]
+
+    def test__is_help__with_long_help_flag__true(self):
+        helper = MockHelper(["arg1", self.HELP_LONG, "arg2"])
+        self.assertTrue(helper.is_help())
+
+    def test__is_help__with_short_help_flag__true(self):
+        helper = MockHelper(["arg1", self.HELP_SHORT, "arg2"])
+        self.assertTrue(helper.is_help())
+
+    def test__is_help__without_help_flag__false(self):
+        helper = MockHelper(["arg1", "arg2", "arg3"])
+        self.assertFalse(helper.is_help())
+
+    def test__help_message__correct_message(self):
+        helper = MockHelper(["arg1", self.HELP_LONG, "arg2"])
+        expected_message = "\n".join(self.HELP_MESSAGE_LINES)
+        self.assertEqual(expected_message, helper.help_messge())
+
+class MockHelper(Helper):
+    HELP_MESSAGE_LINES = TestHelper.HELP_MESSAGE_LINES
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
