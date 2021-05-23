18d9beec812e1ec0453b37ba1d5f01abb070f71b
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-23 01:31:18 +0100

    ADD `Parser` class to `parser` module
    This is a new way of parsing cli args using `argparse`

---
+++ qual_id/parser/parser.py
@@ -0,0 +1,13 @@
+from .argument_parser import ArgumentParser
+from .info_arguments import InfoArguments
+from .main_arguments import MainArguments
+from .command import Command
+
+class Parser:
+    @staticmethod
+    def parse(arguments):
+        parser = ArgumentParser()
+        namespace = parser.parse(arguments)
+        if namespace.command == Command.MAIN:
+            return MainArguments(namespace)
+        return InfoArguments(namespace)
