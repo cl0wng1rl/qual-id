7d919ad117e54f768932a595d6fd6ff08ddeaded
Gabriel Barker <gabrielbarker@gmail.com>
2021-05-24 19:27:50 +0100

    REFACTOR `ArgumentParser` to include custom action

--- qual_id/cli/parser/argument_parser.py
+++ qual_id/cli/parser/argument_parser.py
@@ -1,5 +1,4 @@
-from argparse import ArgumentParser as ArgParser, Namespace
-from .range_action_factory import RangeActionFactory
+from argparse import ArgumentParser as ArgParser, Namespace, Action
 from .command import Command
 from qual_id.groups import GroupFactory
 from qual_id.groups.all import All
@@ -34,7 +33,7 @@ class ArgumentParser:
 
     @staticmethod
     def _add_main_arguments_to(parser) -> None:
-        parser.add_argument('-c', '--categories', action=RangeActionFactory.get_action(1,5), nargs='+', choices=list(All.info()), metavar="CATEGORY")
+        parser.add_argument('-c', '--categories', action=ArgumentParser.RequiredLength, nargs='+', choices=list(All.info()), metavar="CATEGORY")
         parser.add_argument('-g', '--group', default='all', choices=list(GroupFactory.info()), metavar="GROUP")
         parser.add_argument('-f', '--format', default='csv', choices=['csv', 'json'], metavar="FORMAT")
         parser.add_argument('-n', '--number', default=1, type=int)
@@ -44,3 +43,16 @@ class ArgumentParser:
         parser.add_argument('-c', '--category', nargs='?', default=False, choices=list(All.info()), metavar="CATEGORY")
         parser.add_argument('-g', '--group', nargs='?', default=False, choices=list(GroupFactory.info()), metavar="GROUP")
         parser.add_argument('-f', '--format', action='store_true')
+
+    class RequiredLength(Action):
+        MINIMUM=1
+        MAXIMUM=5
+
+        def __call__(self, parser, args, values, option_string=None):
+            if not (self.MINIMUM <= len(values) <= self.MAXIMUM):
+                parser.error(self._get_error_message())
+            setattr(args, self.dest, values)
+
+        def _get_error_message(self):
+            template = 'argument "{arg}" requires between {min} and {max} arguments'
+            return template.format(arg=self.dest, min=self.MINIMUM, max=self.MAXIMUM)
